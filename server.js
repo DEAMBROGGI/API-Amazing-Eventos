2fb55f9f1723dadda3364f5dce684b09ccd98736
Adrian Deambroggi <62616924+DEAMBROGGI@users.noreply.github.com>
2023-08-22 01:04:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); 
 const Router = require('./routes/routes') 
 require('./config/database')
 const app = express();
